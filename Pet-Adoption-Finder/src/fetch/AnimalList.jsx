411756efb76f7ca8e7eb217eb363c02517185087
josh <aaronbaon1@gmail.com>
2024-11-01 18:55:27 +0800

    feat: add AnimalList and BreedList components with basic structure

---
+++ Pet-Adoption-Finder/src/fetch/AnimalList.jsx
@@ -0,0 +1,5 @@
+const AnimalList = () => {
+  return <div></div>;
+};
+
+export default AnimalList;
---
+++ Pet-Adoption-Finder/src/fetch/BreedList.jsx
@@ -0,0 +1,5 @@
+const BreedList = () => {
+  return <div></div>;
+};
+
+export default BreedList;
