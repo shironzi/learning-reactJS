349326d9a73f8e3dc04bed873fbee942e36f585e
josh <aaronbaon1@gmail.com>
2024-11-10 20:41:59 +0800

    create dashboards for admin

---
+++ frontend/src/components/admin/Dashboard.jsx
@@ -0,0 +1,16 @@
+import PetsIcon from "@mui/icons-material/Pets";
+
+function Admin() {
+  return (
+    <div>
+      <div className="cards">
+        <div className="custom-card">
+          <h1>Total Pets</h1>
+          <div className="card-content">10</div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Admin;
--- frontend/src/components/layout/App.jsx
+++ frontend/src/components/layout/App.jsx
@@ -1,42 +1,58 @@
 import { BrowserRouter, Routes, Route, useNavigate } from "react-router-dom";
 import React, { Suspense, useEffect } from "react";
-
-import "./App.css";
-import Header from "../Header";
 import { useDispatch } from "react-redux";
-import { getToken, logout } from "../../apis/auth";
+import { getToken } from "../../apis/auth";
+import "./App.css";
+import { Provider } from "react-redux";
+import store from "../../store";
+import ErrorBoundary from "../ErrorBoundary";
+import Admin from "../admin/Dashboard";
 
 const LazyHome = React.lazy(() => import("../Home"));
 const LazyDetails = React.lazy(() => import("../Details"));
 const LazyLogin = React.lazy(() => import("../Login"));
 const LazyRegister = React.lazy(() => import("../Register"));
 const LazyAddPet = React.lazy(() => import("../AddPet"));
+const LazyHeader = React.lazy(() => import("../Header"));
 
 function App() {
+  return (
+    <Provider store={store}>
+      <BrowserRouter>
+        <ErrorBoundary>
+          <Suspense fallback={<div>Loading...</div>}>
+            <MainRoutes />
+          </Suspense>
+        </ErrorBoundary>
+      </BrowserRouter>
+    </Provider>
+  );
+}
+
+function MainRoutes() {
   const navigate = useNavigate();
   const dispatch = useDispatch();
 
   useEffect(() => {
     const token = getToken();
     if (!token) {
-      dispatch(logout());
+      dispatch({ type: "user/logout" });
       navigate("/auth/login");
     }
   }, [navigate, dispatch]);
 
   return (
-    <BrowserRouter>
-      <Header />
-      <Suspense fallback={<div>Loading...</div>}>
-        <Routes>
-          <Route path="/" element={<LazyHome />} />
-          <Route path="/add-pet" element={<LazyAddPet />} />
-          <Route path="/pets/:id" element={<LazyDetails />} />
-          <Route path="/auth/login" element={<LazyLogin />} />
-          <Route path="/auth/register" element={<LazyRegister />} />
-        </Routes>
-      </Suspense>
-    </BrowserRouter>
+    <>
+      <LazyHeader />
+      <Routes>
+        <Route path="/" element={<LazyHome />} />
+        <Route path="/admin" element={<Admin />} />
+        <Route path="/add-pet" element={<LazyAddPet />} />
+        <Route path="/pets/:id" element={<LazyDetails />} />
+        <Route path="/auth/login" element={<LazyLogin />} />
+        <Route path="/auth/register" element={<LazyRegister />} />
+      </Routes>
+    </>
   );
 }
 
