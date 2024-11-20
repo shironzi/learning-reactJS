d854d19db2a0013cd7e6b5ec88c8f340086400f2
josh <aaronbaon1@gmail.com>
2024-11-20 20:39:09 +0800

    add Loading component

---
+++ frontend/src/components/Loading.jsx
@@ -0,0 +1,9 @@
+function Loading() {
+  return (
+    <div className="loading-container">
+      <img src="/loading-paws.gif" alt="Loading" className="loading-image" />
+    </div>
+  );
+}
+
+export default Loading;
